41982ef03e020a09df5567a5540b20e2f31212dc
Kathy Hoang <kathyhahoang@gmail.com>
2023-07-24 10:16:11 -0500

    Allows user to upload image

--- src/services/supabase.js
+++ src/services/supabase.js
@@ -1,6 +1,6 @@
 import { createClient } from "@supabase/supabase-js";
 
-const supabaseUrl = "https://fzbwhgfblyyekzsosrla.supabase.co";
+export const supabaseUrl = "https://fzbwhgfblyyekzsosrla.supabase.co";
 const supabaseKey =
   "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImZ6YndoZ2ZibHl5ZWt6c29zcmxhIiwicm9sZSI6ImFub24iLCJpYXQiOjE2ODk5NjQ2NDcsImV4cCI6MjAwNTU0MDY0N30.gZsadAukXjXl4ncE_DT1PeqaI-mXlwKPbgmTQ23vYxI";
 const supabase = createClient(supabaseUrl, supabaseKey);
